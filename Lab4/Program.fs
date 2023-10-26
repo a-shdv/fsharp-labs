9b7b7769ea8054fa9f9c364e1cb679fe5390de5e
a-shdv <shadaev2001@icloud.com>
2023-10-26 07:31:03 +0400

    Fix lab4

--- Lab4/Program.fs
+++ Lab4/Program.fs
@@ -1,38 +1,42 @@
 ﻿type IAccountManagement =
-    abstract member OpenAccount : string -> decimal -> unit
-    abstract member CloseAccount : string -> unit
-    abstract member Deposit : string -> decimal -> unit
-    abstract member Withdraw : string -> decimal -> unit
+    abstract member OpenAccount: string -> decimal -> unit
+    abstract member CloseAccount: string -> unit
+    abstract member Deposit: string -> decimal -> unit
+    abstract member Withdraw: string -> decimal -> unit
 
 type IBankCustomer =
-    abstract member GetName : string
-    abstract member GetId : string
+    abstract member Name: string
+    abstract member Id: string
 
 type BankCustomer(name: string, id: string) =
     interface IBankCustomer with
-        member this.GetName = name
-        member this.GetId = id
+        member this.Name = name
+        member this.Id = id
 
-type BankEmployee(name: string, id: string) =
+type BankEmployee(name: string, id: string, isHappy: bool) =
     inherit BankCustomer(name, id)
+    let m_isHappy = isHappy
+    member this.isHappy = m_isHappy
 
 type BankAccount(accountId: string, initialBalance: decimal ref) =
-    member this.GetAccountId = accountId
-    member this.GetBalance = initialBalance
-    
+    member this.AccountId = accountId
+    member this.Balance = initialBalance
+
     new() = BankAccount(null, ref 0.0M)
-    
+
     interface IAccountManagement with
         member this.OpenAccount (accountId: string) (initialBalance: decimal) =
             printfn "Открыт новый счет %s" accountId
-        member this.CloseAccount (accountId: string) =
-            printfn "Счет %s закрыт" accountId
+
+        member this.CloseAccount(accountId: string) = printfn "Счет %s закрыт" accountId
+
         member this.Deposit (accountId: string) (amount: decimal) =
-            this.GetBalance := !this.GetBalance + amount
+            this.Balance := !this.Balance + amount
             printfn "На счет %s зачислено %.2f" accountId amount
+
         member this.Withdraw (accountId: string) (amount: decimal) =
-            if amount <= !this.GetBalance then
-                this.GetBalance := !this.GetBalance - amount
+            if amount <= !this.Balance then
+                this.Balance := !this.Balance - amount
                 printfn "Со счета %s снято %.2f" accountId amount
             else
                 printfn "Недостаточно средств на счете %s" accountId
@@ -41,43 +45,47 @@ type BankAccount(accountId: string, initialBalance: decimal ref) =
 type BankCustomerDepartment() =
     let customers = System.Collections.Generic.Dictionary<string, IBankCustomer>()
 
-    member this.AddCustomer (customer: IBankCustomer) =
-        customers.Add(customer.GetId, customer)
-        printfn "Добавлен новый клиент: %s" customer.GetName
+    member this.AddCustomer(customer: IBankCustomer) =
+        customers.Add(customer.Id, customer)
+        printfn "Добавлен новый клиент: %s" customer.Name
 
-    member this.GetCustomer (customerId: string) =
+    member this.GetCustomer(customerId: string) =
         if customers.ContainsKey(customerId) then
             Some(customers.[customerId])
         else
             None
 
     member this.OpenAccountForCustomer (customer: IBankCustomer) (accountId: string) (initialBalance: decimal ref) =
-        match this.GetCustomer(customer.GetId) with
+        match this.GetCustomer(customer.Id) with
         | Some(customer) ->
-            printfn "Открывается счет для клиента: %s" customer.GetName
+            printfn "Открывается счет для клиента: %s" customer.Name
             let account = BankAccount(accountId, initialBalance)
             account :> IAccountManagement
         | None ->
-            printfn "Клиент с ID %s не найден" customer.GetId
+            printfn "Клиент с ID %s не найден" customer.Id
             let account = BankAccount()
             account :> IAccountManagement
 
 // Пример использования
-let customer1 = BankCustomer("Иванов Иван", "12345")
-let customer2 = BankEmployee("Петров Петр", "67890")
+let customer = BankCustomer("Иванов Иван", "12345")
+let employee = BankEmployee("Петров Петр", "67890", true)
 
 let department = BankCustomerDepartment()
-department.AddCustomer customer1
-department.AddCustomer customer2
+department.AddCustomer customer
+department.AddCustomer employee
 printf "\n"
 
-let initialBalance1 = ref 1000.0M
-let customer1Account = department.OpenAccountForCustomer customer1 "A123" initialBalance1
-customer1Account.Deposit "A123" 500.0M
-customer1Account.Withdraw "A123" 200.0M
+let initialBalanceClient = ref 1000.0M
+let customerAccount =
+    department.OpenAccountForCustomer customer "A123" initialBalanceClient
+customerAccount.Deposit "A123" 500.0M
+customerAccount.Withdraw "A123" 200.0M
 printf "\n"
 
-let initialBalance2 = ref 2000.0M
-let customer2Account = department.OpenAccountForCustomer customer2 "B456" initialBalance2
-customer2Account.CloseAccount "B456"
+let initialBalanceEmployee = ref 2000.0M
+let employeeAccount =
+    department.OpenAccountForCustomer employee "B456" initialBalanceEmployee
+employeeAccount.CloseAccount "B456"
+let mood = employee.isHappy = false
+printf "isHappy: %A" mood
 printf "\n"
