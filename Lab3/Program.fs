f471f2d95c95e6a8f6c21e00c2803cbe39e53f2f
a-shdv <shadaev2001@icloud.com>
2023-11-02 03:42:41 +0400

    Fix lab3

--- Lab3/Program.fs
+++ Lab3/Program.fs
@@ -1,25 +1,5 @@
 ﻿// В массиве все нечетные элементы, стоящие после максимального, увеличить в 10 раз.
-// Пример: из массива A[5]: 3 7 1 5 4 должен получиться массив 3 7 10 50 40.
-
-// open Microsoft.FSharp.Collections
-//
-// [<EntryPoint>]
-// let main argv =
-//     let arr = [| 3; 1; 1; 7; 1; 5; 4 |]
-//
-//     let max = arr
-//               |> Array.max
-//
-//     let index = arr
-//                 |> Array.findIndex (fun x -> x = max)
-//
-//     arr.[index + 1 ..]// не должен создаваться срез (через цикл for)
-//
-//     |> Array.map (fun x -> x * 10)
-//     |> Array.append arr.[0..index]
-//     |> printf "%A "
-//
-//     0
+// Пример: из массива A[5]: 3 7 1 5 4 должен получиться массив 3 7 10 50 4.
 
 open Microsoft.FSharp.Collections
 
@@ -27,20 +7,18 @@ open Microsoft.FSharp.Collections
 let main argv =
     let arr = [| 3; 1; 1; 7; 1; 5; 4 |]
 
-    let max = arr |> Array.max
-    let index = arr |> Array.findIndex (fun x -> x = max)
-
-    arr
-    |> Array.iter (fun x -> printf "%d " x)
-    printf "\n"
+    let maxEl = arr |> Array.max
+    let indexMaxEl = arr |> Array.findIndex (fun i -> i = maxEl)
     
-    for i = index + 1 to arr.Length - 1 do
-        arr.[i] <- arr.[i] * 10
+    let printArray arr =
+        arr |> Array.map string |> String.concat " " |> printfn "%s"
+
+    printArray arr
 
     arr
-    |> Array.iter (fun x -> printf "%d " x)
+    |> Array.iteri (fun i el ->
+        if i > indexMaxEl && el % 2 <> 0 then
+            arr.[i] <- el * 10)
 
+    printArray arr
     0
-
-
-
